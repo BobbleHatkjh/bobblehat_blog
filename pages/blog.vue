72a40c8fad5988e1136bd859d1de0e52a87d731e
BobbleHatkjh <3206633623@qq.com>
2020-11-15 20:01:24 +0800

    -new- ·直接进行一个sh的pu
    -to do-

--- pages/blog.vue
+++ pages/blog.vue
@@ -1,7 +1,25 @@
 <template>
   <div class="blog_bac">
-    <h2>博客 迁移中...</h2>
-    <Button @click="jumpCSDN" :scroll="true" icon="go">去看原博客</Button>
+
+    <div class="blog_side_bar">
+
+    </div>
+
+    <div class="blog_body">
+
+      <div class="blog_header">
+        <i class="iconfont icon-fold"/>
+        <p>绒球帽的博客</p>
+      </div>
+
+      <div class="blog_content">
+        <h2>博客 迁移中...</h2>
+        <Button @click="jumpCSDN" :scroll="true" icon="go">去看原博客</Button>
+      </div>
+
+
+    </div>
+
 
   </div>
 </template>
@@ -12,7 +30,7 @@ export default {
     Button: () => import(/* webpackChunkName: "button_animate" */ '~/components/Button')
   },
   methods: {
-    jumpCSDN(){
+    jumpCSDN() {
       window.open('https://blog.csdn.net/BobbleHat')
     }
   }
@@ -21,21 +39,62 @@ export default {
 
 <style>
 .blog_bac {
+  display: flex;
+  align-items: center;
   min-height: 100vh;
+  height: 100vh;
+  background-color: white;
+}
+
+.blog_side_bar {
+  display: flex;
+  height: calc(100% - 70px);
+  width: 280px;
+  /*box-shadow: 4px 0 4px -4px #dadada;*/
+  border-right: 1px solid #dddddd;
+}
+
+.blog_body {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  flex: 1;
+  height: 100%;
+}
+
+.blog_header {
+  display: flex;
+  height: 66px;
+  align-items: center;
+  width: calc(100% - 60px);
+  font-size: 18px;
+  color: #5c5c5c;
+  /*box-shadow: 0 4px 4px -4px #dadada;*/
+  border-bottom: 1px solid #dddddd;
+}
+.blog_header i{
+  font-size: 22px;
+  margin-right: 18px;
+  transition: color 0.3s, transform 0.3s;
+}
+.blog_header i:hover{
+  cursor: pointer;
+  color: #ff4c10;
+  /*transform: rotate(180deg);*/
+}
+.blog_header i:active{
+  transform: scale(0.8);
+}
+
+
+.blog_content {
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  transform: translate3d(0, 0, 0);
-  background-color: white;
+  width: 100%;
+  flex: 1;
 }
 
-@keyframes appear {
-  0% {
-    opacity: 0;
-    /*margin-top: 80px;*/
 
-    transform: translateY(90px);
-  }
-}
 </style>
